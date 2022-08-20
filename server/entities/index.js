b777bb3aa34eaa6e8ec7af1f4025bc0fa7be7609
andy-liuu <aliu3160@gmail.com>
2022-08-20 14:52:28 -0400

    remove server

--- server/entities/index.js
+++
@@ -1,9 +0,0 @@
-import OfficeSchema from "./OfficeSchema";
-import ProductSchema from "./ProductSchema";
-import RatingSchema from "./RatingSchema";
-
-export default {
-  OfficeSchema,
-  ProductSchema,
-  RatingSchema,
-};
