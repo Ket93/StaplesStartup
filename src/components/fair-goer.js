dd72ea002af19346d49f30d262fb2a6108011f96
iivyleii <iivyleii@gmail.com>
2022-08-20 18:54:41 -0400

    idk man banner thing

--- src/components/fair-goer.js
+++ src/components/fair-goer.js
@@ -6,7 +6,7 @@ import ALB from '../images/ALB.png';
 import BC from '../images/BC.png';
 import ON from '../images/ON.png';
 import STUDIO from '../images/staples-studio.png';
-import STARTUP from '../images/happy-startups.jpg';
+import STARTUP from '../images/happy-startup.jpg';
 import MARKER from '../images/location-marker.png';
 import { Button} from 'react-bootstrap';
 import { Link } from 'react-router-dom'
@@ -14,10 +14,11 @@ import { Link } from 'react-router-dom'
 export const FairGoer = () => {
   return (
     <>
-      <img src={STUDIO} id='studio'/>
-      <div className="shade"></div>
-      <img src={STARTUP} id='startup'/>
-      <h1 className="title">Welcome to <span className="red">Staples Startup</span></h1>
+      <div className="header">
+        <img src={STARTUP} id='startup'/>
+        <img src={STUDIO} id='studio'/>
+        <h1 className="title"><span className="white">Welcome to</span><span className="red">Staples Startup</span></h1>
+      </div>
       <Link to="./#location">
 	        <Button> Find Location </Button>
 	    </Link>
@@ -25,12 +26,28 @@ export const FairGoer = () => {
         <img src={BC} id='bc'/>
         <img src={ALB} id='alb'/>
         <img src={ON} id='on'/>
-        <img src={MARKER} id='marker1'/>
-        <img src={MARKER} id='marker2'/>
-        <img src={MARKER} id='marker3'/>
-        <img src={MARKER} id='marker4'/>
-        <img src={MARKER} id='marker5'/>
-        <img src={MARKER} id='marker6'/>
+
+        <a href="/calgary" id='63010eb7e4d31a1b84ce37b4'>
+          <img src={MARKER} id='marker1'/>
+        </a>
+        <a href="/kelowna" id='63010eb7e4d31a1b84ce37b4'>
+          <img src={MARKER} id='marker2'/>
+        </a>
+        <a href="/oakville" id='63010eb7e4d31a1b84ce37b4'>
+          <img src={MARKER} id='marker3'/>
+        </a>
+        <a href="/ottawa" id='63010eb7e4d31a1b84ce37b4'>
+          <img src={MARKER} id='marker4'/>
+        </a>
+        <a href="/corktown" id='63010eb7e4d31a1b84ce37b4'>
+          <img src={MARKER} id='marker5'/>
+        </a>
+        <a href="/midtown" id='63010eb7e4d31a1b84ce37b4'>
+          <img src={MARKER} id='marker6'/>
+        </a>
+        <a href="/university" id='63010eb7e4d31a1b84ce37b4'>
+          <img src={MARKER} id='marker7'/>
+        </a>
       </div>
       <h2 id='location' className="titleChooseLocation">Choose a Location</h2>
     </>
